030d62a43709972aefde4721b544d558b96345aa
Adefokun Adeoluwa <adefokunadeoluwaisrael@gmail.com>
2024-12-06 15:12:59 +0100

    update

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -40,6 +42,5 @@ export default {
       "sunset",
     ],
   },
-  // eslint-disable-next-line no-undef
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
